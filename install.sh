74370824670c9f39c2cb33c527a1bff07254e895
Nandagopal SB <nandagopal.nsb@gmail.com>
2025-04-13 21:48:56 +0530

    added files

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt install sway
+sudo apt install lf 
+sudo apt install ffmpeg
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+
